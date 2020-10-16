145bfd3c3a91c1d3f2accf7d62cb5fc58b990fed
Phi-001 <71786223+Phi-001@users.noreply.github.com>
2020-10-16 08:28:24 -0700

    Update PGL.1.1.1.js

--- PGL.1.1.1.js
+++ PGL.1.1.1.js
@@ -1,1 +1,14 @@
-
+var PGL;
+var gl;
+(function() {
+	PGL.init = function(canvas) {
+		var glArgs = {preserveDrawingBuffer : true, failIfMajorPerformanceCaveat : false};
+		gl = canvas.getContext("webgl", glArgs) || canvas.getContext("experimental-webgl", glArgs);
+		if (!PGL.gl) {
+			alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+			return;
+		}
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+	};
+});
