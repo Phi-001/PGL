4b52a92560129af5e2ef16e4d65a02c1d71546b2
Phi-001 <71786223+Phi-001@users.noreply.github.com>
2020-10-18 11:11:39 -0700

    Update PGL.1.1.1.js

--- PGL.1.1.1.js
+++ PGL.1.1.1.js
@@ -6,6 +6,8 @@ on html
 var PGL;
 (function() {
 	var canvas, gl;
+	var textureUnit = 0;
+	var initialized = false;
 	// sets up canvas and WebGL
 	var init = function() {
 		if (canvas) {
@@ -21,6 +23,7 @@ var PGL;
 		}
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
+		initialized = true;
 	};
 	// initialize shader from it's code and type
 	var initShader = function(sourceCode, shaderType) {
@@ -48,37 +51,165 @@ var PGL;
 		}
 		return program;
 	};
-	// initialize element buffer from array
-	var initElementBuffer = function(array) {
-		const buffer = gl.createBuffer();
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(array), gl.STATIC_DRAW);
-		return buffer;
-	};
-	// initialize array buffer from array
-	var initArrayBuffer = function(array) {
-		const buffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array), gl.STATIC_DRAW);
-		return buffer;
-	};
 	// binds attributes
 	var bindAttribs = function(programInfo, buffers) {
 		// set indicies
-		gl.bindbuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.index);
+		gl.bindbuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.index.buffer);
 		// set every attributes
 		for (var i in buffers) {
-			gl.bindBuffer(gl.ARRAY_BUFFER, buffers[i].buffer);
-			gl.vertexAtribPointer(programInfo.attribLocations[i], buffers[i].numComponents, buffers[i].type, false, 0, 0);
-			gl.enableVertexAttribArray(programInfo.attribLocations[i]);
+			if (buffers.hasOwnProperty(i)) {
+				gl.bindBuffer(gl.ARRAY_BUFFER, buffers[i].buffer);
+				gl.vertexAtribPointer(programInfo.attribLocations[i], buffers[i].numComponents, buffers[i].type, false, 0, 0);
+				gl.enableVertexAttribArray(programInfo.attribLocations[i]);
+			}
 		}
 	};
+	// gets bind point for sampler type
+	function getBindPointForSamplerType(type) {
+		if (type === gl.SAMPLER_2D) {
+			return gl.TEXTURE_2D;        
+		}
+		if (type === gl.SAMPLER_CUBE) {
+			return gl.TEXTURE_CUBE_MAP;  
+		}
+		return undefined;
+	}
 	// sets uniform
-	var setUniform = function(programInfo) {
-		
+	var uniformSetter = function(program, uniformInfo) {
+		const location = gl.getUniformLocation(program, uniformInfo.name);
+		const type = uniformInfo.type;
+		// Check if this uniform is an array
+		const isArray = (uniformInfo.size > 1 && uniformInfo.name.substr(-3) === '[0]');
+		if (type === gl.FLOAT && isArray) {
+			return function(v) {
+				gl.uniform1fv(location, v);
+			};
+		}
+		if (type === gl.FLOAT) {
+			return function(v) {
+				gl.uniform1f(location, v);
+			};
+		}
+		if (type === gl.FLOAT_VEC2) {
+			return function(v) {
+				gl.uniform2fv(location, v);
+			};
+		}
+		if (type === gl.FLOAT_VEC3) {
+			return function(v) {
+				gl.uniform3fv(location, v);
+			};
+		}
+		if (type === gl.FLOAT_VEC4) {
+			return function(v) {
+				gl.uniform4fv(location, v);
+			};
+		}
+		if (type === gl.INT && isArray) {
+			return function(v) {
+				gl.uniform1iv(location, v);
+			};
+		}
+		if (type === gl.INT) {
+			return function(v) {
+				gl.uniform1i(location, v);
+			};
+		}
+		if (type === gl.INT_VEC2) {
+			return function(v) {
+				gl.uniform2iv(location, v);
+			};
+		}
+		if (type === gl.INT_VEC3) {
+			return function(v) {
+				gl.uniform3iv(location, v);
+			};
+		}
+		if (type === gl.INT_VEC4) {
+			return function(v) {
+				gl.uniform4iv(location, v);
+			};
+		}
+		if (type === gl.BOOL) {
+			return function(v) {
+				gl.uniform1iv(location, v);
+			};
+		}
+		if (type === gl.BOOL_VEC2) {
+			return function(v) {
+				gl.uniform2iv(location, v);
+			};
+		}
+		if (type === gl.BOOL_VEC3) {
+			return function(v) {
+				gl.uniform3iv(location, v);
+			};
+		}
+		if (type === gl.BOOL_VEC4) {
+			return function(v) {
+				gl.uniform4iv(location, v);
+			};
+		}
+		if (type === gl.FLOAT_MAT2) {
+			return function(v) {
+				gl.uniformMatrix2fv(location, false, v);
+			};
+		}
+		if (type === gl.FLOAT_MAT3) {
+			return function(v) {
+				gl.uniformMatrix3fv(location, false, v);
+			};
+		}
+		if (type === gl.FLOAT_MAT4) {
+			return function(v) {
+				gl.uniformMatrix4fv(location, false, v);
+			};
+		}
+		if ((type === gl.SAMPLER_2D || type === gl.SAMPLER_CUBE) && isArray) {
+			const units = [];
+			for (var i = 0; i < uniformInfo.size; i++) {
+				units.push(textureUnit++);
+			}
+			return function(bindPoint, units) {
+				return function(textures) {
+					gl.uniform1iv(location, units);
+					for (var i = 0; i < textures.length; i++) {
+						gl.activeTexture(gl.TEXTURE0 + units[i]);
+						gl.bindTexture(bindPoint, textures[i]);
+					}
+				};
+			}(getBindPointForSamplerType(type), units);
+		}
+		if (type === gl.SAMPLER_2D || type === gl.SAMPLER_CUBE) {
+			return function(bindPoint, unit) {
+				return function(texture) {
+					gl.uniform1i(location, unit);
+					gl.activeTexture(gl.TEXTURE0 + unit);
+					gl.bindTexture(bindPoint, texture);
+				};
+			}(getBindPointForSamplerType(type), textureUnit++);
+		}
+	};
+	// sets uniform to its corresponding value
+	var setUniforms = function(programInfo) {
+		textureUnit = 0;
+		const program = programInfo.program;
+		const numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+		for (var i = 0; i < numUniforms; i++) {
+			const uniformInfo = gl.getActiveUniform(program, i);
+			if (!uniformInfo) {
+				break;
+			}
+			var name = uniformInfo.name;
+			if (name.substr(-3) === '[0]') {
+				name = name.substr(0, name.length - 3);
+			}
+			const setter = uniformSetter(programInfo.program, uniformInfo);
+			setter(programInfo.uniforms.name);
+		}
 	};
 	// draws Object in the scene
-	var drawObject = function(programInfo, cameraInfo, objectInfo, buffers) {
+	var drawScene = function(programInfo, buffers) {
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.clearDepth(1.0);
 		gl.enable(gl.DEPTH_TEST);
@@ -88,10 +219,60 @@ var PGL;
 		bindAttribs(programInfo, buffers);
 		// uses program
 		gl.useProgram(programInfo.program);
-		// constats for uniforms
-		for (var i in programInfo.uniforms) {
-			
-		}
+		// sets uniforms
+		setUniforms(programInfo);
+		// draw them
+		gl.drawElements(gl.TRIANGLES, programInfo.index.length, gl.UNSIGNED_SHORT, 0);
 	};
-	
+	// renderes scene from program information
+	var render = function(programInfo) {
+		if (!initialized) {
+			init();
+		}
+		const program = initProgram(programInfo.vertexShader, programInfo.fragmentShader);
+		programInfo.program = program;
+		const numAtribs = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+		var buffers = {};
+		// indicies
+		{
+			const buffer = gl.createBuffer();
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(programInfo.index), gl.STATIC_DRAW);
+			buffers.index = {
+				buffer: buffer
+			};
+		}
+		// other attributes
+		for (var i = 0; i < numAtribs; i++) {
+			const atribInfo = gl.getActiveAtrib(program, i);
+			if (!atribInfo) {
+				break;
+			}
+			var name = atribInfo.name;
+			var type = atribInfo.type;
+			if (name.substr(-3) === '[0]') {
+				name = name.substr(0, name.length - 3);
+			}
+			const buffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(programInfo.attributes[name]), gl.STATIC_DRAW);
+			var numComponents = type.substr(-1);
+			if (numComponents === '2') {
+				numComponents = 2;
+			} else if (numComponents === '3') {
+				numComponents = 3;
+			} else if (numComponents === '4') {
+				numComponents = 4;
+			} else {
+				numComponents = 1;
+			}
+			buffers[name] = {
+				buffer: buffer,
+				numComponents: numComponents,
+				type: type,
+			};
+		}
+		drawScene(programInfo, buffers);
+	};
 })();
+	
