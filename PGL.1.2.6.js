4a432bade79bb33161728ed1aba4773a79828a81
Phi-001 <71786223+Phi-001@users.noreply.github.com>
2020-10-31 18:12:24 -0700

    Update PGL.1.2.6.js

--- PGL.1.2.6.js
+++ PGL.1.2.6.js
@@ -289,7 +289,7 @@ var PGL;
 		image.onload = function() {
 			gl.bindTexture(gl.TEXTURE_2D, texture);
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-			if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+			if (isPowerOf2(image.width) && isPowerOf2(image.height) && image.width === image.height) {
 				gl.generateMipmap(gl.TEXTURE_2D);
 			} else {
 				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -305,7 +305,7 @@ var PGL;
 		const texture = gl.createTexture();
 		gl.bindTexture(gl.TEXTURE_2D, texture);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(data));
-		if (isPowerOf2(info.width) && isPowerOf2(info.height)) {
+		if (isPowerOf2(info.width) && isPowerOf2(info.height) && info.width === info.height) {
 			gl.generateMipmap(gl.TEXTURE_2D);
 		} else {
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
